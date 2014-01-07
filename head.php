67fd5ac3dc5d8046d74020c26afed19d1bc297b8
ChelseaStats <ChelseaStats@users.noreply.github.com>
2014-01-07 13:53:10 +0000

    Update head.php

--- head.php
+++ head.php
@@ -1,21 +1,23 @@
 <?php
-
-foreach(
-		array(  
-		    'reset.css',
-				'bootstrap.css',
-				'slider.css',
-				'style.css' )
-		as $cssfile){
+if($variant == 'live'){
+?>
+<link rel="stylesheet" type="text/css" href="/css/minified.css" />
+<?php
+	}
+	else {
+		foreach(
+				array(
+				    'reset.css',
+						'bootstrap.css',
+						'slider.css',
+						'style.css' )
+				as $cssfile)
+	  {
 ?>
 
-<link rel="stylesheet" type="text/css" href="/css/styler.php?variant=<?php echo $variant; ?>&amp;sheet=<?php echo $cssfile; ?>" />
-		<?php
-		}
-	}else{
-		?>
-<link rel="stylesheet" type="text/css" href="/css/css_wrapper.php?variant=<?php echo $variant; ?>" />
-		<?php
+	<link rel="stylesheet" type="text/css" href="/css/styler.php?variant=<?php echo $variant; ?>&amp;sheet=<?php echo $cssfile; ?>" />
+
+<?php
 	}
-}
+	  }
 ?>
